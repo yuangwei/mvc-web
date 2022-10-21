a9180a0b8506838deb3dc3a8360e0cb286fd3606
weiyuang <weiyuang@joyy.com>
2022-10-21 14:40:36 +0800

    update

--- config/database.ts
+++ config/database.ts
@@ -2,12 +2,12 @@ export default ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'localhost'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'cms'),
-      user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', '******'),
-      ssl: env.bool('DATABASE_SSL', false),
+      host: env('DATABASE_HOST'),
+      port: env.int('DATABASE_PORT'),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
+      ssl: env.bool('DATABASE_SSL'),
     },
   },
 });
